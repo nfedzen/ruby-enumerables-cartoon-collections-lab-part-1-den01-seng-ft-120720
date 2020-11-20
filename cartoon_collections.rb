167c59f4088d5b8b33a38b5a7aa522d4df6afd2c
Nathan Fedzen <nfedzen@hotmail.com>
2020-11-20 21:17:32 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -3,7 +3,7 @@ def greet_characters(array)
   #
   # Print a custom greeting for each element
   array.each do |character|
-    puts "Hello #{character}"
+    puts "Hello #{character}!"
   end
 end
 
